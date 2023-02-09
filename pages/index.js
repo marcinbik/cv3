1427f6fc805c5cc65ddd7f0976fd200e98a3365a
marcinbik <marcinbik55@gmail.com>
2023-02-09 22:04:30 +0100

    esilnt conf

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,24 @@
-import Head from '../components/Head.js'
+import Head from 'components/Head'
 import Image from 'next/image'
-import Navbar from '@/components/Navbar.js'
-
-
+import Navbar from 'components/Navbar'
 
 export default function Home() {
   return (
     <>
-      <Head/>
-      <Navbar/>
-      <div className="grid container grid-cols-12 mt-5 lg:m-20  ">
-      
+      <Head />
+      <Navbar />
+      <div className="grid container grid-cols-12 grid-rows-12 gap-4 m-3  lg:m-20  ">
         <div className="col-span-4"></div>
-        <div className=' sm:col-span-12 lg:col-span-6'>
-        <h1 className=' font-display font-bold tracking-wide  text-center text-3xl'>
-          Rozwiń skrzydła dla swojego biznesu z naszymi innowacyjnymi rozwiązaniami
-        </h1>
+        <div className=" sm:col-span-12 lg:col-span-6">
+          <h1 className=" font-display font-bold tracking-wide  text-center text-3xl">
+            Rozwiń skrzydła dla swojego biznesu z naszymi innowacyjnymi
+            rozwiązaniami
+          </h1>
         </div>
-
+        <div className="lg:col-span-4  row-span-5 col-span-12 h-[65vh]  overflow-hidden relative z-0 ">
+          <Image src="/heropic.jpg" alt='hero' fill />
+        </div>
       </div>
-      
-      
     </>
-
   )
 }
