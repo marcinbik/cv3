5950c6cf85c046b6741dc26201a5fd9b1a65a16f
marcinbik <marcinbik55@gmail.com>
2023-02-09 22:52:53 +0100

    fix eslint error

--- components/Head.js
+++ components/Head.js
@@ -1,13 +1,10 @@
 import Head from 'next/head'
-    
 
-    
-    export default function head ()
-    {
-        <Head>
-            <title>Marbit</title>
-            <meta name="description" content="Aplikacje dla twojej Firmy" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-    }
+export default function head() {
+  <Head>
+    <title>Marbit</title>
+    <meta name="description" content="Aplikacje dla twojej Firmy" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+}
