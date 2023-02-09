658a0aa2bac030e8f25f8affaecdeea21c949d7b
marcinbik <marcinbik55@gmail.com>
2023-02-09 23:35:02 +0100

    fix eslint error

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'  // eslint-disable-line
 
 export default function Document() {
   return (
