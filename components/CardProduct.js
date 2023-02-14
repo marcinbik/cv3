31499d84875aa5e712480d278d7de4679747589c
marcinbik <marcinbik55@gmail.com>
2023-02-14 15:17:12 +0100

    delete jpg

--- components/CardProduct.js
+++ components/CardProduct.js
@@ -1,6 +1,6 @@
-import Image from 'next/image'
 
-export default function CardProduct({ img, title, text, price }) { // eslint-disable-line
+
+export default function CardProduct({ title, text, price }) {// eslint-disable-line
   const listItems = text.map((element) => (
     <div key={title + element} className="p-4">
       <p>{element}</p>
@@ -8,15 +8,12 @@ export default function CardProduct({ img, title, text, price }) { // eslint-dis
   ))
 
   return (
-    <div className="  bg-gradient-to-r from-primary-900 to-accent-700 rounded-lg col-span-12 ring-4  lg:col-span-4 z-0 m-2  overflow-hidden grid">
-      <div className="aspect-[16/8] object-contain relative overflow-hidden ">
-        <Image src={img} alt={title} fill className=" " />
-      </div>
-      <div className="px-12 text-primary-50 py-16 container grow">
+    <div className=" max-w-lg lg:max-w-xl justify-center bg-gradient-to-r from-primary-900 to-accent-700 rounded-lg col-span-12 ring-4  lg:col-span-4 z-0 m-2  overflow-hidden grid">
+      <div className="px-12  text-primary-50 py-16 container grow">
         <h2 className="text-center text-xl lg:text-3xl font-display font-bold mb-2">
           {title}
         </h2>
-        <p className="mb-8 text-center font-display text-xl lg:text-3xl font-bold text-accent-300">
+        <p className="mb-20 text-center font-display text-xl lg:text-3xl font-bold text-accent-300">
           od {price} zł
         </p>
         <div className="  lg:text-lg">{listItems}</div>
