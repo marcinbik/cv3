5e18f75458c743bbba3079c4f0c6975ead31ee1a
marcinbik <marcinbik55@gmail.com>
2023-02-16 14:52:08 +0100

    edit navbar

--- components/CardProduct.js
+++ components/CardProduct.js
@@ -6,15 +6,15 @@ export default function CardProduct({ title, text, price }) {// eslint-disable-l
   ))
 
   return (
-    <div className=" max-w-md  container mb-10  justify-center bg-gradient-to-r from-primary-900 to-accent-700 rounded-lg col-span-12 ring-4 md:col-span-6 lg:col-span-3 z-0   overflow-hidden grid">
-      <div className="  text-primary-50 py-16 container grow">
+    <div className=" max-w-md mb-10  justify-center bg-gradient-to-r from-primary-700 to-primary-400 rounded-lg col-span-12  md:col-span-6 xl:col-span-3 z-0   overflow-hidden grid">
+      <div className="  text-primary-50 py-16  grow">
         <h2 className="text-center text-xl lg:text-3xl font-display font-bold mb-2">
           {title}
         </h2>
-        <p className="mb-10 text-center font-display text-xl lg:text-3xl font-bold text-accent-300">
+        <p className="mb-10 text-center font-display text-xl lg:text-3xl font-bold text-accent-800">
           od {price} zł
         </p>
-        <div className="lg:text-lg">{listItems}</div>
+        <div className="lg:text-lg px-5">{listItems}</div>
         <p text={text} />
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -24,16 +24,16 @@ export default function Home() {
   return (
     <>
       <Head />
-      <div className="grid grid-cols-12  items-center  justify-center  ">
+      <div className="grid grid-cols-12  items-center gap-2  justify-center  ">
         <div className=" lg:h-fit bg-[url('/road.png')]  bg-cover col-span-12 overflow-hidden  relative z-0 ">
             <Navbar />
-          <div className="flex items-center place-content-center place-items-center justify-center">
+          <div className="flex items-center  place-content-center place-items-center justify-center">
             <HeroBaner />
           </div>
         </div>
       </div>
       <div className="h-30  m-20"></div>
-      <div className="grid  place-items-center grid-cols-12 text-black">
+      <div className="grid  place-items-center mx-auto container grid-cols-12 text-black">
         <CardProduct
           title={'Strona WordPress'}
           price={1499}
