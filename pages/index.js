18efcb64cb1423fa9ade7a5cde9fcda92d2c10d2
marcinbik <marcinbik55@gmail.com>
2023-02-27 20:24:00 +0100

    add img

--- pages/index.js
+++ pages/index.js
@@ -29,40 +29,55 @@ export default function Home() {
       <div className="grid grid-cols-12  items-center gap-2  justify-center  ">
         <div className=" h-screen bg-[url('/road.png')]  bg-cover col-span-12 overflow-hidden  relative z-0 ">
           <Navbar />
-          <div className=" sm:h-0 overflow-hidden  col-span-12 md:h-32 2xl:h-40 "></div>
+          <div className=" sm:h-0 overflow-hidden  col-span-12 md:h-28  "></div>
           <div className="flex items-center  place-content-center place-items-center justify-center">
             <HeroBaner href="#choice" />
           </div>
         </div>
       </div>
-      <div className="col-span-12  text-center text-accent-800  text-6xl my-20 ">
+      <div className="col-span-12  text-center text-accent-800  text-6xl mt-48 my-20 ">
         <h3 id="">Dlaczego warto być w internecie </h3>
       </div>
-      <div className="grid container grid-cols-12 mx-auto  items-center gap-8  justify-center   text-primary-50  mb-28  ">
-        <div className="  justify-center items-center col-span-8 text-2xl rounded-xl  bg-primary-800 p-10 ">
+      <div className="md:grid container grid-cols-12 mx-auto  items-center gap-8  justify-center   text-primary-50  mb-48  ">
+        <div className="  justify-center items-center sm:col-span-12 md:col-span-8 text-2xl rounded-xl opacity-80  bg-gradient-to-r my-5 from-primary-700 to-primary-400 p-20 ">
           <p>
             Strony internetowe i sklepy internetowe są niezbędnymi narzędziami
             dla każdej firmy, która chce dotrzeć do swoich klientów w
-            dzisiejszych czasach.<br/> Dzięki nim możesz zaprezentować swoje produkty
-            lub usługi w atrakcyjny sposób, dotrzeć do szerszej grupy odbiorców,
-            a także zwiększyć swoją sprzedaż.<br/> Posiadanie własnej strony
-            internetowej to nie tylko sposób na zwiększenie widoczności Twojej
-            firmy w sieci, ale również idealna okazja do nawiązania
-            bezpośredniego kontaktu z klientami.<br/><br/> Dzięki stronie internetowej
-            możesz przedstawić swoją ofertę, udzielać informacji o swoich
-            produktach, a także prowadzić bloga i publikować interesujące
-            treści.<br/> Natomiast sklep internetowy to idealne rozwiązanie dla firm,
-            które chcą sprzedawać swoje produkty przez Internet.<br/> Dzięki niemu
-            klienci mogą łatwo i wygodnie złożyć zamówienie na Twoje produkty, a
-            Ty możesz w prosty sposób zarządzać swoimi zapasami i realizować
-            zamówienia.<br/><br/> Nie czekaj więc dłużej i zacznij działać już teraz!
-            Zaufaj profesjonalistom i załóż swoją własną stronę internetową lub
-            sklep internetowy.<br/><br/> Z naszą pomocą
-            Twoja firma będzie mogła odnosić jeszcze większe sukcesy!
+            dzisiejszych czasach.
+            <br /> Dzięki nim możesz zaprezentować swoje produkty lub usługi w
+            atrakcyjny sposób, dotrzeć do szerszej grupy odbiorców, a także
+            zwiększyć swoją sprzedaż.
+            <br /> Posiadanie własnej strony internetowej to nie tylko sposób na
+            zwiększenie widoczności Twojej firmy w sieci, ale również idealna
+            okazja do nawiązania bezpośredniego kontaktu z klientami.
+            <br />
+            <br /> Dzięki stronie internetowej możesz przedstawić swoją ofertę,
+            udzielać informacji o swoich produktach, a także prowadzić bloga i
+            publikować interesujące treści.
+            <br /> Natomiast sklep internetowy to idealne rozwiązanie dla firm,
+            które chcą sprzedawać swoje produkty przez Internet.
+            <br /> Dzięki niemu klienci mogą łatwo i wygodnie złożyć zamówienie
+            na Twoje produkty, a Ty możesz w prosty sposób zarządzać swoimi
+            zapasami i realizować zamówienia.
+            <br />
+            <br /> Nie czekaj więc dłużej i zacznij działać już teraz! Zaufaj
+            profesjonalistom i załóż swoją własną stronę internetową lub sklep
+            internetowy.
+            <br />
+            <br />
+            <b>
+              Z naszą pomocą Twoja firma będzie mogła odnosić jeszcze większe
+              sukcesy!
+            </b>
           </p>
         </div>
-        <div className=" col-span-4 mt-40  bg-accent-700 rounded-full p-auto justify-center   w-full aspect-video relative">
-          <Image src="/person.png" fill/>
+        <div className="  col-span-4 mt-40 flex flex-col   items-center   z-50    justify-center   aspect-square   relative">
+          <Image
+            className=" z-0  bg-accent-200 rounded-full w-24 h-24 "
+            src="/person.png"
+            fill
+            alt="person"
+          />
         </div>
       </div>
       <div className="col-span-12 text-center text-accent-800 text-6xl mb-20 ">
