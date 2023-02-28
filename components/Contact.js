a4ca83ef91090b809a33a6eb0c1c662f9893abc8
marcinbik <marcinbik55@gmail.com>
2023-02-28 15:01:21 +0100

    refactoring

--- components/Contact.js
+++ components/Contact.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Contact() {
   return (
     <div className="bg-primary-200">
-      <div className=" md:grid grid-cols-12 p-10 bg-primary-200 sm:w-fit md:w-max mx-auto rounded-xl justify-items-center items-center justify-center text-primary-900 container font-semibold  text-lg lg:text-xl 2xl:text-4xl   ">
+      <div className=" md:grid grid-cols-12  p-10 bg-primary-200 sm:w-fit md:w-max mx-auto rounded-xl justify-items-center items-center justify-center text-primary-900 container font-semibold  text-lg lg:text-xl 2xl:text-4xl   ">
         <div className=" sm:col-span-12  place-items-center  aspect-video relative  md:col-span-4">
           <Image
             src="/blackLogo.png"
@@ -15,15 +15,13 @@ export default function Contact() {
             height={300}
           />
         </div>
-        <div className="sm:col-span-12  md:col-span-3 hover:text-accent-900 transition-all   ">
-          <div className="">
+        <div className="sm:col-span-12  md:col-span-4 hover:text-accent-900 transition-all   ">
             <FontAwesomeIcon icon={faPhone} />
-          </div>
           <Link href="tel:887407337" className="px-2">
             887407337
           </Link>
         </div>
-        <div className="sm:col-span-12   md:col-span-5 hover:text-accent-900 transition-all   ">
+        <div className="sm:col-span-12 md:col-span-4 hover:text-accent-900 transition-all   ">
           <FontAwesomeIcon icon={faEnvelope} />
           <Link href="mailto:marcinbikiewicz@gmail.com" className="px-2">
             marcinbikiewicz@gmail
