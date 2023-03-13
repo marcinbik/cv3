a320b40096ab954de688d8c52c735d6abe90ea40
marcinbik <marcinbik55@gmail.com>
2023-03-13 21:59:13 +0100

    edit

--- pages/services.js
+++ pages/services.js
@@ -13,9 +13,8 @@ export default function services() {
       </div>
       <div className="mt-20 grid-cols-12 gap-4 container mx-auto">
         <Lead text={'Opis Usług'} />
-        <div className="  sm:col-span-12 md:col-span-8 ">
+        <div className="  sm:col-span-12 md:col-span-8 rounded-xl  bg-cover bg-[url('/wordpress.jpg')]" >
           <ProductDescription
-            img={'/wordpress.jpg'}
             title={'Strona Wordpress dla twojej Firmy'}
             lead={'Prostota obsługi szybka realizacja'}
             list={[
